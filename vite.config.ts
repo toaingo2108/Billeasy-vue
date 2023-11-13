a91ea50ecfc59ef8244ff25aaf80489f97e7e98d
Toai Quoc Ngo <toaingoquoc00@gmail.com>
2023-11-13 12:05:32 -0400

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -6,40 +6,59 @@ import ViteFonts from "unplugin-fonts/vite";
 // Utilities
 import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "node:url";
+import { sanitizeFileName } from "./vite/util";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-      styles: {
-        configFile: "src/styles/settings.scss",
+export default defineConfig(({ command, mode }) => {
+  const isBuild: boolean = command === "build";
+
+  return {
+    plugins: [
+      vue({
+        template: { transformAssetUrls },
+      }),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+      vuetify({
+        autoImport: true,
+        styles: {
+          configFile: "src/styles/settings.scss",
+        },
+      }),
+      ViteFonts({
+        google: {
+          families: [
+            {
+              name: "Roboto",
+              styles: "wght@100;300;400;500;700;900",
+            },
+          ],
+        },
+      }),
+    ],
+    define: { "process.env": {} },
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
       },
-    }),
-    ViteFonts({
-      google: {
-        families: [
-          {
-            name: "Roboto",
-            styles: "wght@100;300;400;500;700;900",
-          },
-        ],
-      },
-    }),
-  ],
-  define: { "process.env": {} },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
     },
-    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
-  },
-  server: {
-    port: 3000,
-  },
-  base: "/Billeasy-vue/",
+    server: {
+      port: 3000,
+    },
+    base: isBuild ? "/Billeasy-vue/" : "",
+    build: {
+      outDir: "./dist/",
+      rollupOptions: {
+        emptyOutDir: true,
+        output: {
+          manualChunks(id: string) {
+            if (id.includes("node_modules")) {
+              return id.split("/node_modules/").pop()?.split("/")[0];
+            }
+          },
+          sanitizeFileName,
+        },
+      },
+    },
+  };
 });
